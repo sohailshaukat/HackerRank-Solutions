642c46207ce75b260f7636fd7da75d4749a1cdde
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-08-11 19:17:22 +0530

    Python Gold Batch Achievedgit add .

---
+++ Incorrect Regex/ir.py
@@ -0,0 +1,14 @@
+'''
+problem URL: https://www.hackerrank.com/challenges/incorrect-regex/problem
+-sohailshaukat ( https://github.com/sohailshaukat )
+-sohail47k@gmail.com
+'''
+import re
+for _ in range(int(input())):
+    validity = True
+    reg = input()
+    try:
+        reg = re.compile(reg)
+    except re.error:
+        validity = False
+    print(validity)
